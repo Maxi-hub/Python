fef63fb0db1c48a1a8c9354899a33586b486f207
Emma <em.lebedeva85@gmail.com>
2023-07-28 12:52:45 +0300

    Added octal into NumberSystems

--- HomeWork4/NumberSystems.py
+++ HomeWork4/NumberSystems.py
@@ -7,13 +7,13 @@
 '''
 
 
-def get_number():
+def get_number() -> int:
     number = int(input("Введите число: "))
     return number
 
 number = get_number()
 
-def get_binary_number(number: int):
+def get_binary_number(number: int) -> list:
     list1 = []
     remainder = 0
     while number > 0:
@@ -25,7 +25,7 @@ def get_binary_number(number: int):
 
 list2 = get_binary_number(number)
 
-def list_flip(list: list):
+def list_flip(list: list) -> list:
     for i in range(len(list2) // 2): # перемещение конечного элемента списка на начальный и наоборот, доходя до середины списка 
         list2[i], list2[len(list2)-1-i] = list2[len(list2)-1-i], list2[i]
     return list2
@@ -33,8 +33,29 @@ def list_flip(list: list):
 list2 = list_flip(list2)
 
 binary_number = int(''.join(map(str, list2)))
-print(f"Число в восьмеричной системе представления: {binary_number}") 
+print(f"Число в двоичной системе представления: {binary_number}") 
 
 print(bin(number))
+
+
+'''
+Для перевода из двоичной в восьмеричную систему разбиваем двоичное число на группы по 3 цифры справа налево. 
+В последней (самой левой) группе вместо недостающих цифр ставим слева нули.
+'''
+while len(list2)%3 != 0: # дополняем список нулями
+    list2.insert(0, 0)
+
+new_list = []
+for i in range(0, len(list2), 3):
+    one = list2[i]*(2**2)
+    two = list2[i+1]*(2**1)
+    three = list2[i+2]*(2**0)
+    res = one + two + three
+    new_list += {res}
+
+octal_number = int(''.join(map(str, new_list)))
+print(f"Число в восьмеричной системе представления: {octal_number}") 
+
+print(oct(number))
 
 
